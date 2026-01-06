a2ad405e279129a35b5913ac5f82883b37d0f5f7
Swiftly01 <kareemkazeem100@gmail.com>
2026-01-06 15:27:58 +0100

    add other pages

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,12 +1,20 @@
 import Navigation from "@/components/Navigation";
 import HeroSection from "@/components/HeroSection";
 import Footer from "@/components/Footer";
+import ServicesSection from "@/components/ServicesSection";
+import ProjectsSection from "@/components/ProjectsSection";
+import AboutSection from "@/components/AboutSection";
+import ContactSection from "@/components/ContactSection";
 
 const Index = () => {
   return (
-    <div className="min-h-screen bg-background overflow-x-hidden">
+    <div className="min-h-screen overflow-x-hidden bg-background">
       <Navigation />
       <HeroSection />
+      <ServicesSection />
+      <ProjectsSection />
+      <AboutSection />
+      <ContactSection />
       <Footer />
     </div>
   );
